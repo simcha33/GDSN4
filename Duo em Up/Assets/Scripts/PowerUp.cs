31962df145b05142d0de85c75fad51992e32164d
simcha33 <36536375+simcha33@users.noreply.github.com>
2019-05-09 17:13:03 +0200

    power up system implemented

---
+++ Duo em Up/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private PowerUpClasses powerUpClasses; 
+
+    public string _powerUpName;
+    public GameObject _appearance;
+    public float _movementSpeed;
+    public float _deSpawnTime;
+
+    //influencers 
+    public float _healthChange;
+    public float _damageChange; 
+    public bool _tagChange; 
+
+    public int index; 
+
+    void Start()
+    {
+         powerUpClasses = GameObject.Find("Manager").GetComponent<PowerUpClasses>();    
+         _powerUpName = powerUpClasses.PowerUpsList[index].powerUpName;
+         _appearance = powerUpClasses.PowerUpsList[index].appearance; 
+         _movementSpeed = powerUpClasses.PowerUpsList[index].movementSpeed; 
+         _deSpawnTime = powerUpClasses.PowerUpsList[index].deSpawnTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _deSpawnTime-=Time.deltaTime;
+        if (_deSpawnTime <=0 ){ 
+            DestroyStuff(); 
+        }
+
+        //variables from the class script    
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if ((other.gameObject.tag == "Player" && _deSpawnTime > 0) || (other.gameObject.tag == "Player Projectile" && _deSpawnTime > 0)) {
+            ActivatePowerUp(); 
+        }
+    }
+
+    void ActivatePowerUp(){
+        Debug.Log("power up picked up"); 
+        DestroyStuff(); 
+    }
+
+    void DestroyStuff(){
+        Destroy(gameObject); 
+    }
+}
---
+++ Duo em Up/Assets/Scripts/PowerUpClasses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Events;
+
+
+public class PowerUpClasses : MonoBehaviour
+{
+    public List<PowerUpsList> PowerUpsList;
+    
+}
+
+[System.Serializable]
+public class PowerUpsList
+{
+    public string powerUpName;
+    public GameObject appearance; 
+    public float movementSpeed; //how fast the item moves through the envoriment
+    public float deSpawnTime; //how long it takes for the item to despawn
+    public float effectTime; //how long does the powerup effect last 
+
+    //influencers 
+    public float healthChange;
+    public float damageChange; 
+    public bool tagChange; 
+}
+
+
+
+
